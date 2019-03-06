7a5aa7301d417d896833e538a7b70244b4db23ef
Caelan <caelan@Caelans-iMac.lan>
2019-03-06 15:04:40 +1100

    android version can now download and display image

--- installer/lib/screen_image.dart
+++ installer/lib/screen_image.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 
 class ImageScreen extends StatefulWidget {
   ImageScreen({Key key}) : super(key: key);
-
 
   @override
   _ImageScreenState createState() => _ImageScreenState();
@@ -10,33 +13,59 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   bool _imageLoaded = false;
+  File _remoteImage;
+
+  Future<File> downloadFile(String fileName) async {
+    print("Downloading image: $fileName from FirebaseStorage..");
+    Directory tempDir = Directory.systemTemp;
+    final File file = File('${tempDir.path}/$fileName');
+
+    final StorageReference ref = FirebaseStorage.instance.ref().child(fileName);
+    final StorageFileDownloadTask downloadTask = ref.writeToFile(file);
+
+    downloadTask.future.then((snapshot) {
+      setState(() {
+        _imageLoaded = true;
+        _remoteImage = file;
+      });
+    });
+
+    return file;
+  }
+
+  @override
+  void initState() {
+    downloadFile("remote_image.jpg");
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: null,
-            body: Center(
-              child: !_imageLoaded
-        ? Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text("Fetching image from camera\n\n",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.grey[500], fontSize: 16.0),
-),            
-              CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(
-                    Theme.of(context).primaryColor),
-              ),
-            ],
-          ))
-        : Column(
+      appBar: null,
+      body: Center(
+        child: !_imageLoaded
+            ? Center(
+                child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[],
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Fetching image from camera\n\n",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey[500], fontSize: 16.0),
+                  ),
+                  CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                        Theme.of(context).primaryColor),
+                  ),
+                ],
+              ))
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Image.file(_remoteImage)],
               ),
-            ),
-          );
+      ),
+    );
   }
 }
