03dbef030f336710a6b23588c3d1fe4229861e44
Caelan <caelan@Caelans-iMac.lan>
2019-03-17 17:04:03 +1100

    percentage widget and some of library list done

--- userapp/lib/screen_choose_library.dart
+++ userapp/lib/screen_choose_library.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:percent_indicator/percent_indicator.dart';
 import 'screen_map_view.dart';
 import 'main.dart';
 
@@ -33,8 +33,17 @@ class _ChooseLibraryScreenState extends State<ChooseLibraryScreen> {
         onWillPop: () async => true,
         child: Scaffold(
           appBar: AppBar(
-            title: const Text('Libraries'),
+            backgroundColor: Theme.of(context).canvasColor,
+            title: const Text(
+              'Libraries',
+              style: TextStyle(color: Colors.grey),
+            ),
             centerTitle: true,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              color: Colors.grey,
+              onPressed: () => Navigator.pop(context),
+            ),
             actions: <Widget>[],
           ),
           body: Navigator(onGenerateRoute: (RouteSettings settings) {
@@ -43,6 +52,58 @@ class _ChooseLibraryScreenState extends State<ChooseLibraryScreen> {
             });
           }),
         ));
+  }
+
+  Widget _buildPercentageWidget(int totalSeats, int totalPeople) {
+    int percentageFull = (100*(totalPeople / totalSeats)).toInt();
+    Color color = percentageFull < 25
+        ? Colors.green
+        : percentageFull < 50
+            ? Colors.yellow
+            : percentageFull < 75 ? Colors.orange : Colors.red;
+
+    return CircularPercentIndicator(
+      radius: 60.0,
+      lineWidth: 5.0,
+      percent: percentageFull / 100.0,
+      center: new Text("$percentageFull%"),
+      progressColor: color,
+    );
+  }
+
+  Widget _buildLibraryTile(
+      String libraryTitle, String libraryID, int totalSeats, int totalPeople) {
+    return Card(
+      child: ListTile(
+        leading: _buildPercentageWidget(totalSeats, totalPeople),
+        title: Text(
+          libraryTitle,
+          style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 36.0,
+              color: Colors.grey[600]),
+        ),
+        subtitle: Text("$totalSeats seats",
+            style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 16.0,
+                color: Colors.grey[600])),
+        trailing: Icon(Icons.arrow_forward_ios),
+        onTap: () {
+          //  Store selection info
+          _chosenLibrary = libraryID;
+
+          Navigator.of(context, rootNavigator: true).push(
+            MaterialPageRoute(
+              builder: (context) => MapScreen(
+                    library: _chosenLibrary,
+                    libraryTitle: libraryTitle,
+                  ),
+            ),
+          );
+        },
+      ),
+    );
   }
 
   Widget _buildFirebaseList(collectionPath) {
@@ -78,33 +139,10 @@ class _ChooseLibraryScreenState extends State<ChooseLibraryScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        ListTile(
-                          // dense: true,
-                          // padding: EdgeInsets.all(20.0),
-                          // color: index % 2 == 0 ? Colors.grey[200] : Colors.grey[0],
-                          title: Text(
-                            document['title'],
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 20.0,
-                                color: Colors.grey[600]),
-                          ),
-                          onTap: () {
-                            //  Store selection info
-                            _chosenLibrary = document.documentID;
-
-                            Navigator.of(context, rootNavigator: true).push(
-                              MaterialPageRoute(
-                                builder: (context) => MapScreen(
-                                      library: _chosenLibrary,
-                                      libraryTitle: document['title'],
-                                    ),
-                              ),
-                            );
-                          },
-                        ),
-                        Divider(),
+                        _buildLibraryTile("Baileuu", "baileuu", 400, 225),
+                        _buildLibraryTile("Arts", "baileuu", 400, 370),
+                        _buildLibraryTile("Brownless", "baileuu", 400, 170),
+                        _buildLibraryTile("ERC", "baileuu", 400, 90),
                       ],
                     );
                   }).toList(),
